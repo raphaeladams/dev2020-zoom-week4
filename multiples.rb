4642b1a9a47fa6048351d6110da1fd976d859283
Raphael Adams <raphael.adams@shopify.com>
2020-10-23 13:52:36 -0400

    Called method by itself, and using two blocks

--- multiples.rb
+++ multiples.rb
@@ -4,7 +4,7 @@
 def customized_list_of_multiples(multiples_of, size)
 
   list_of_multiples = Array.new(size)
-  list_of_multiples.each_index { |i| list_of_multiples[i] = multiples_of * i }
+  list_of_multiples.map! { |i| multiples_of * list_of_multiples.index(i) }
 
   if block_given? 
     yield(list_of_multiples)
@@ -19,3 +19,16 @@ list_test1 = customized_list_of_multiples(2, 5)
 print list_test1
 print "\n"
 
+list_test2 = customized_list_of_multiples(2, 5) do |list|
+  puts "first block"
+  list.map! { |i| i.to_s + " converted to String"}
+end
+print list_test2
+print "\n"
+
+list_test3 = customized_list_of_multiples(2, 5) do |list|
+  puts "second block"
+  list.map! { |i| i * 1000 }
+end
+print list_test3
+print "\n"
