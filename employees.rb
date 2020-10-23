ff5bbdc64398c65a073e99826142dfc193e468b2
Raphael Adams <raphael.adams@shopify.com>
2020-10-23 11:57:27 -0400

    Added class methods for HourlyEmployee

--- employees.rb
+++ employees.rb
@@ -5,15 +5,15 @@ class Employee
 
   attr_reader :name
 
-  def initialize(name = "Anonymous")
-    self.name = name
-  end
-
   def name=(name)
     if name == ""
       raise "Name can't be blank"
     end
     @name = name
+  end
+
+  def initialize(name = "Anonymous")
+    self.name = name
   end
 
   def print_name
@@ -27,17 +27,17 @@ class SalariedEmployee < Employee
 
     attr_reader :salary
 
-    def initialize(name = "Anonymous", salary = 0.0)
-      super(name)
-      self.salary = salary
-    end
-
     def salary=(salary)
       if salary < 0
         raise "A salary of #{ salary } isn't valid"
       end
       @salary = salary
     end
+
+    def initialize(name = "Anonymous", salary = 0.0)
+      super(name)
+      self.salary = salary
+    end  
 
     def print_pay_stub
       print_name
@@ -51,13 +51,19 @@ end
 
 class HourlyEmployee < Employee
 
-    attr_reader :hourly_wage, :hours_per_week
-
-    def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
-      super(name)
-      self.hourly_wage = hourly_wage
-      self.hours_per_week = hours_per_week
+    def self.temp(name)
+      HourlyEmployee.new(name, 10.50, 20)
     end
+  
+    def self.salesman(name)
+      HourlyEmployee.new(name, 12.75, 25)
+    end
+  
+    def self.accountant(name)
+      HourlyEmployee.new(name, 19.25, 30)
+    end
+  
+    attr_reader :hourly_wage, :hours_per_week
 
     def hourly_wage=(hourly_wage)
       if hourly_wage < 0
@@ -73,9 +79,10 @@ class HourlyEmployee < Employee
       @hours_per_week = hours_per_week
     end
 
-    def turn_into_cashier
-      self.hourly_wage = 12.75
-      self.hours_per_week = 25
+    def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+      super(name)
+      self.hourly_wage = hourly_wage
+      self.hours_per_week = hours_per_week
     end
 
     def print_pay_stub
@@ -88,23 +95,17 @@ class HourlyEmployee < Employee
 end
 
 
-salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
-salaried_employee.print_pay_stub
+jim = HourlyEmployee.salesman("Jim Halpert")
+dwight = HourlyEmployee.salesman("Dwight Schrute")
+pam = HourlyEmployee.salesman("Pam Beesly")
+stanley = HourlyEmployee.salesman("Stanley Hudson")
 
-hourly_employee = HourlyEmployee.new("John Smith", 14.97, 30)
-hourly_employee.print_pay_stub
+ryan = HourlyEmployee.temp("Ryan Howard")
 
+kevin = HourlyEmployee.accountant("Kevin Malone")
+angela = HourlyEmployee.accountant("Angela Martin")
+oscar = HourlyEmployee.accountant("Oscar Martinez")
 
-jim = HourlyEmployee.new("Jim Halpert")
-jim.turn_into_cashier
 jim.print_pay_stub
-
-dwight = HourlyEmployee.new("Dwight Schrute", 12.75, 25)
-pam = HourlyEmployee.new("Pam Beesly", 12.75, 25)
-ryan = HourlyEmployee.new("Ryan Howard", 12.75, 25)
-
-stanley = HourlyEmployee.new("Stanley Hudson", 10.50, 20)
-kevin = HourlyEmployee.new("Kevin Malone", 10.50, 20)
-
-angela = HourlyEmployee.new("Angela Martin", 19.25, 30)
-oscar = HourlyEmployee.new("Oscar Martinez", 19.25, 30)
+ryan.print_pay_stub
+angela.print_pay_stub
