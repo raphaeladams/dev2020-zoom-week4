058dcc918137e70a351f62852c9c3cb3296a1727
Raphael Adams <raphael.adams@shopify.com>
2020-10-23 09:28:16 -0400

    Added overriding Talk methods for Cat & Bird

--- animal.rb
+++ animal.rb
@@ -38,7 +38,13 @@ class Dog < Animal
 end
 
 class Bird < Animal
+  def talk
+    puts "#{ @name } says Chirp Chirp!"
+  end
 end
 
 class Cat < Animal
+  def talk
+    puts "#{ @name } says Meow!"
+  end
 end
